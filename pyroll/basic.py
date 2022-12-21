379f0ee83ffab6444f049daf62706998ca385ca4
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-12-21 14:39:46 +0100

    upd to 2.0a

--- pyroll/basic.py
+++ pyroll/basic.py
@@ -1,8 +1,10 @@
 import pyroll.core as core
-from pyroll.core import solve, BoxGroove, ConstrictedBoxGroove, SquareGroove, DiamondGroove, RoundGroove, \
-    FalseRoundGroove, CircularOvalGroove, FlatOvalGroove, SwedishOvalGroove, ConstrictedSwedishOvalGroove, \
-    Oval3RadiiGroove, Oval3RadiiFlankedGroove, SplineGroove, GenericElongationGroove, FlatGroove, Transport, \
-    RollPass, Unit, Roll, Profile, MaxIterationCountExceededError
+from pyroll.core import (
+    BoxGroove, ConstrictedBoxGroove, SquareGroove, DiamondGroove, RoundGroove, FalseRoundGroove,
+    CircularOvalGroove, FlatOvalGroove, SwedishOvalGroove, ConstrictedSwedishOvalGroove, Oval3RadiiGroove,
+    Oval3RadiiFlankedGroove, SplineGroove, GenericElongationGroove, FlatGroove, Transport, RollPass, Unit, Roll,
+    Profile, Rotator, PassSequence, Hook, HookHost, HookFunction
+)
 
 import pyroll.freiberg_flow_stress as freiberg_flow_stress
 from pyroll.freiberg_flow_stress import FreibergFlowStressCoefficients
@@ -12,3 +14,6 @@ import pyroll.hensel_power_and_labour as hensel_power_and_labour
 import pyroll.wusatowski_spreading as wusatowski_spreading
 import pyroll.hitchcock_roll_flattening as hitchcock_roll_flattening
 import pyroll.lendl_equivalent_method as lendl_equivalent_method
+
+import pyroll.report as report
+import pyroll.export as export
